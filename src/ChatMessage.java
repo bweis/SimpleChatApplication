2efe1e13af0890616903797edddf50bba6e25b95
Benjamin Weis <bweis96@gmail.com>
2017-10-24 23:26:40 -0400

    Tabbed

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,23 +1,23 @@
-import java.io.*;
+    import java.io.*;
 
-public class ChatMessage implements Serializable {
-    protected static final long serialVersionUID = 6898543889087L;
+    public class ChatMessage implements Serializable {
+        protected static final long serialVersionUID = 6898543889087L;
 
-    static final int MESSAGE = 0, LOGOUT = 1;
-    private int type;
-    private String message;
+        static final int MESSAGE = 0, LOGOUT = 1;
+        private int type;
+        private String message;
 
-    ChatMessage(int type, String message) {
-        this.type = type;
-        this.message = message;
+        ChatMessage(int type, String message) {
+            this.type = type;
+            this.message = message;
+        }
+
+        int getType() {
+            return type;
+        }
+
+        String getMessage() {
+            return message;
+        }
     }
-
-    int getType() {
-        return type;
-    }
-
-    String getMessage() {
-        return message;
-    }
-}
-// Test
+    // Test
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,173 +1,173 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+    import java.io.IOException;
+    import java.io.ObjectInputStream;
+    import java.io.ObjectOutputStream;
+    import java.net.ServerSocket;
+    import java.net.Socket;
+    import java.text.SimpleDateFormat;
+    import java.util.ArrayList;
+    import java.util.Date;
 
-public class ChatServer {
-    private static int uniqueId;
-    private ArrayList<ClientThread> clients = new ArrayList<>();
-    private SimpleDateFormat sdf;
-    private int port;
+    public class ChatServer {
+        private static int uniqueId;
+        private ArrayList<ClientThread> clients = new ArrayList<>();
+        private SimpleDateFormat sdf;
+        private int port;
 
 
-    private ChatServer(int port) {
-        this.port = port;
-        sdf = new SimpleDateFormat("HH:mm:ss");
-    }
-
-    private void start() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-
-            while (true) {
-                display("Server waiting for Clients on port " + port + ".");
-                Socket socket = serverSocket.accept();
-                Runnable r = new ClientThread(socket);
-                Thread t = new Thread(r);
-                clients.add((ClientThread) r);
-                t.start();
-            }
-        } catch (IOException e) {
-            String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
-            display(msg);
+        private ChatServer(int port) {
+            this.port = port;
+            sdf = new SimpleDateFormat("HH:mm:ss");
         }
-    }
 
-    private void display(String msg) {
-        String time = sdf.format(new Date()) + " " + msg;
-        System.out.println(time);
-    }
+        private void start() {
+            try {
+                ServerSocket serverSocket = new ServerSocket(port);
 
-    private synchronized void broadcast(String message) {
-        String time = sdf.format(new Date());
-        String formattedMessage = time + " " + message + "\n";
-        System.out.print(formattedMessage);
-
-        for (ClientThread clientThread : clients) {
-            if (!clientThread.writeMsg(formattedMessage)) {
-                clients.remove(clients.indexOf(clientThread));
-                display("Disconnected Client " + clientThread.username + " removed from list.");
+                while (true) {
+                    display("Server waiting for Clients on port " + port + ".");
+                    Socket socket = serverSocket.accept();
+                    Runnable r = new ClientThread(socket);
+                    Thread t = new Thread(r);
+                    clients.add((ClientThread) r);
+                    t.start();
+                }
+            } catch (IOException e) {
+                String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
+                display(msg);
             }
         }
-    }
 
-    private synchronized void remove(int id) {
-        for(int i = 0; i < clients.size(); ++i) {
-            if(clients.get(i).id == id) {
-                clients.remove(i);
-                return;
+        private void display(String msg) {
+            String time = sdf.format(new Date()) + " " + msg;
+            System.out.println(time);
+        }
+
+        private synchronized void broadcast(String message) {
+            String time = sdf.format(new Date());
+            String formattedMessage = time + " " + message + "\n";
+            System.out.print(formattedMessage);
+
+            for (ClientThread clientThread : clients) {
+                if (!clientThread.writeMsg(formattedMessage)) {
+                    clients.remove(clients.indexOf(clientThread));
+                    display("Disconnected Client " + clientThread.username + " removed from list.");
+                }
             }
         }
-    }
 
-    public static void main(String[] args) {
-        int portNumber = 1500;
-        switch (args.length) {
-            case 1:
-                try {
-                    portNumber = Integer.parseInt(args[0]);
-                } catch (Exception e) {
-                    System.out.println("Invalid port number.");
-                    System.out.println("Usage is: > java Server [portNumber]");
+        private synchronized void remove(int id) {
+            for(int i = 0; i < clients.size(); ++i) {
+                if(clients.get(i).id == id) {
+                    clients.remove(i);
                     return;
                 }
-            case 0:
-                break;
-            default:
-                System.out.println("Usage is: > java Server [portNumber]");
-                return;
-
-        }
-
-        ChatServer server = new ChatServer(portNumber);
-        server.start();
-    }
-
-    class ClientThread implements Runnable {
-        Socket socket;
-        ObjectInputStream sInput;
-        ObjectOutputStream sOutput;
-        int id;
-        String username;
-        ChatMessage cm;
-        String date;
-
-        ClientThread(Socket socket) {
-            id = ++uniqueId;
-            this.socket = socket;
-            try {
-                sOutput = new ObjectOutputStream(socket.getOutputStream());
-                sInput = new ObjectInputStream(socket.getInputStream());
-                username = (String) sInput.readObject();
-                display(username + " just connected.");
-            } catch (IOException e) {
-                display("Exception creating new Input/output Streams: " + e);
-                return;
-            } catch (ClassNotFoundException ignored) {
             }
-            date = new Date().toString() + "\n";
         }
 
-        public void run() {
-            boolean isRunning = true;
-            while (isRunning) {
-                try {
-                    cm = (ChatMessage) sInput.readObject();
-                } catch (Exception e) {
-                    display(username + " Exception reading Streams: " + e);
+        public static void main(String[] args) {
+            int portNumber = 1500;
+            switch (args.length) {
+                case 1:
+                    try {
+                        portNumber = Integer.parseInt(args[0]);
+                    } catch (Exception e) {
+                        System.out.println("Invalid port number.");
+                        System.out.println("Usage is: > java Server [portNumber]");
+                        return;
+                    }
+                case 0:
                     break;
-                }
+                default:
+                    System.out.println("Usage is: > java Server [portNumber]");
+                    return;
 
-                String message = cm.getMessage();
-
-                // Switch on the type of message receive
-                switch (cm.getType()) {
-                    case ChatMessage.MESSAGE:
-                        broadcast(username + ": " + message);
-                        break;
-                    case ChatMessage.LOGOUT:
-                        display(username + " disconnected with a LOGOUT message.");
-                        isRunning = false;
-                        break;
-                }
             }
-            remove(id);
-            close();
+
+            ChatServer server = new ChatServer(portNumber);
+            server.start();
         }
 
-        private void close() {
-            try {
-                if (sOutput != null) sOutput.close();
-            } catch (IOException ignored) {
-            }
-            try {
-                if (sInput != null) sInput.close();
-            } catch (Exception ignored) {
-            }
-            try {
-                if (socket != null) socket.close();
-            } catch (Exception ignored) {
-            }
-        }
+        class ClientThread implements Runnable {
+            Socket socket;
+            ObjectInputStream sInput;
+            ObjectOutputStream sOutput;
+            int id;
+            String username;
+            ChatMessage cm;
+            String date;
 
-        private boolean writeMsg(String msg) {
-            if (!socket.isConnected()) {
+            ClientThread(Socket socket) {
+                id = ++uniqueId;
+                this.socket = socket;
+                try {
+                    sOutput = new ObjectOutputStream(socket.getOutputStream());
+                    sInput = new ObjectInputStream(socket.getInputStream());
+                    username = (String) sInput.readObject();
+                    display(username + " just connected.");
+                } catch (IOException e) {
+                    display("Exception creating new Input/output Streams: " + e);
+                    return;
+                } catch (ClassNotFoundException ignored) {
+                }
+                date = new Date().toString() + "\n";
+            }
+
+            public void run() {
+                boolean isRunning = true;
+                while (isRunning) {
+                    try {
+                        cm = (ChatMessage) sInput.readObject();
+                    } catch (Exception e) {
+                        display(username + " Exception reading Streams: " + e);
+                        break;
+                    }
+
+                    String message = cm.getMessage();
+
+                    // Switch on the type of message receive
+                    switch (cm.getType()) {
+                        case ChatMessage.MESSAGE:
+                            broadcast(username + ": " + message);
+                            break;
+                        case ChatMessage.LOGOUT:
+                            display(username + " disconnected with a LOGOUT message.");
+                            isRunning = false;
+                            break;
+                    }
+                }
+                remove(id);
                 close();
-                return false;
             }
-            try {
-                sOutput.writeObject(msg);
-            } catch (IOException e) {
-                display("Error sending message to " + username);
-                display(e.toString());
+
+            private void close() {
+                try {
+                    if (sOutput != null) sOutput.close();
+                } catch (IOException ignored) {
+                }
+                try {
+                    if (sInput != null) sInput.close();
+                } catch (Exception ignored) {
+                }
+                try {
+                    if (socket != null) socket.close();
+                } catch (Exception ignored) {
+                }
             }
-            return true;
+
+            private boolean writeMsg(String msg) {
+                if (!socket.isConnected()) {
+                    close();
+                    return false;
+                }
+                try {
+                    sOutput.writeObject(msg);
+                } catch (IOException e) {
+                    display("Error sending message to " + username);
+                    display(e.toString());
+                }
+                return true;
+            }
         }
     }
-}
 
-// Test
+    // Test
